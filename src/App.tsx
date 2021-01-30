8cdd2a8d1229d63174f4365ce50e724bcc2e73cf
Daniel Tonon <dan503.npm@gmail.com>
2021-01-30 22:16:55 +1100

    Adding credit for personality trait and quirk tables

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ function App() {
 				<FlexGap direction="vertical">
 					<RollResult label="Race" tableData={races} dSize={'d100'} />
 					<RollResult label="Sex" tableData={sex} dSize={'d4'} />
-					<p>Use the Fantasy Name Generator app to generate character names.</p>
+					<p>
+						Use the "Fantasy Name Generator" app to generate character names.
+					</p>
 					<RollResult
 						label="Personality trait"
 						tableData={personality}
@@ -20,6 +22,13 @@ function App() {
 					/>
 					<RollResult label="Quirk" tableData={quirks} dSize={'d100'} />
 					<RollResult label="Age (appearance)" dSize={'d100'} />
+					<p>
+						Personality traits and Quirks from{' '}
+						<a href="https://www.dmsguild.com/product/317982/PC-and-NPC-Creation-Tables">
+							PC and NPC Creation Tables
+						</a>{' '}
+						on DMs Guild
+					</p>
 				</FlexGap>
 			</div>
 		</div>
--- src/data/npcs/personality.ts
+++ src/data/npcs/personality.ts
@@ -1,5 +1,5 @@
 import { d100, RollTable } from "../../utils/utilTypes";
-
+/** Personality traits from [PC and NPC Creation Tables](https://www.dmsguild.com/product/317982/PC-and-NPC-Creation-Tables) on DMs Guild */
 export const personality: RollTable<d100> = [
 	{
 		roll: 1,
