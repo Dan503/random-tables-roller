abcea49e422cd181d3736f26c0794eb3f983733f
Daniel Tonon <dan503.npm@gmail.com>
2021-01-30 17:57:07 +1100

    first data set (races)

---
+++ src/data/index.ts
@@ -0,0 +1,1 @@
+export { races } from './races'
---
+++ src/data/races.ts
@@ -0,0 +1,36 @@
+import { RollTable } from "../utils/utilTypes";
+
+export const races: RollTable = [
+	{
+		roll: [1, 5],
+		value: 'Human'
+	},
+	{
+		roll: [6, 10],
+		value: 'Dwarf'
+	},
+	{
+		roll: [11, 15],
+		value: 'Goliath'
+	},
+	{
+		roll: 16,
+		value: 'Elf'
+	},
+	{
+		roll: 17,
+		value: 'Halfling'
+	},
+	{
+		roll: 18,
+		value: 'Tiefling'
+	},
+	{
+		roll: 19,
+		value: 'Dragonborn'
+	},
+	{
+		roll: 20,
+		value: 'Half-orc'
+	}
+]
