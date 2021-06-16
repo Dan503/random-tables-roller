de6f97334aab9fe1b25abbbc0562fc153c9938c0
Daniel Tonon <dan503.npm@gmail.com>
2021-06-17 00:43:57 +1000

    Updating encounters text

--- src/data/Predefined-Encounters/travelEncounters.ts
+++ src/data/Predefined-Encounters/travelEncounters.ts
@@ -1,22 +1,22 @@
 export const dangerousTravelEncounters = [
 	// 1
-	'The “Creature of Nightmares” finds the party (see above explanation)',
+	'The "Spine Demon" (Abominable Yeti) finds the party', // "Spine" as in "Spine of the World" mountain range
 	// 2
 	'Avalanche or Blizzard',
 	// 3
-	'Creature(s) try to lure the party into an avalanche to eat their frozen bodies (animal bait/ chase after them?)',
+	'Creature(s) try to lure the party into an avalanche to eat their frozen bodies (animal bait / chase after them?)',
 	// 4
 	'Ice elementals are the source of a blizzard, must be defeated to stop blizzard',
 	// 5
 	'A song echoes through the mountains / wilderness toward people around a campfire (Wisdom save) (good or bad with enchantment music?)',
 	// 6
-	'A creature hiding in wait un-burrows itself/ breaks through ice and surprise attacks one unlucky PC',
+	'A creature hiding in wait un-burrows itself / breaks through ice and surprise attacks one unlucky PC',
 	// 7
 	'A tribe of warriors overwhelm and kidnap the party, can they escape or talk their way out?',
 	// 8
 	'Start to be hunted down by a creature (unaware at first)',
 	// 9
-	'Creature is stalking the group and always retreats when	moved towards, what is it waiting for? (moment to strike/ more to arrive?)',
+	'Creature is stalking the group and always retreats when	moved towards, what is it waiting for? (moment to strike / more to arrive?)',
 	// 10
 	'Start to be followed by an Assassin (unaware at first)',
 	// 11
@@ -33,9 +33,9 @@ export const dangerousTravelEncounters = [
 	`Two huge creatures battle to the death in front of the
 		party (Do they join in?) One (or both) dies and the
 		party is left with its corpse (White Dragon? What do
-		they do with it? Who/ what else wants the carcass?)`,
+		they do with it? Who / what else wants the carcass?)`,
 	// 17
-	'A trap (Dex saving throw) that suspends/ nets/ traps the PC’s in place (who/ what is coming?)',
+	'A trap (Dex saving throw) that suspends / nets/ traps the PC’s in place (who / what is coming?)',
 	// 18
 	'Two dueling wizards keep casting ice spells that block your path from their battle',
 	// 19
@@ -43,25 +43,25 @@ export const dangerousTravelEncounters = [
 	// 20
 	'A Juvenile White Dragon finds the party on its first hunt (Adult dragon watches over it?)',
 	// 21
-	'A huge sleeping creature blocks their travel, can they pass without waking it? (owlbear/ frost giant?)',
+	'A huge sleeping creature blocks their travel, can they pass without waking it? (owlbear / frost giant?)',
 	// 22
-	'Group of villagers fleeing from something chasing them (Single creature/ hoard/ nothing?)',
+	'Group of villagers fleeing from something chasing them (Single creature / hoard / nothing?)',
 	// 23
 	'Two opposing factions fight over treasure (bandits vs wizards / or Barbarians vs druids etc)',
 	// 24
-	'Dangerous creatures aggressively running towards party, but away from something more dangerous (Dragon/ Remorhaz/ Frost Ooze?)',
+	'Dangerous creatures aggressively running towards party, but away from something more dangerous (Dragon / Remorhaz / Frost Ooze?)',
 	// 25
 	'Section of frozen land / lake is between you and your destination (1/2 speed for the day)',
 	// 26
 	'A group of tribal nomads, are they hostile or friendly? (Roll Reaction Table?)',
 	// 27
-	'Everyone makes a Wisdom save or rolls on the “Extreme Cold Mental Table”',
+	'Everyone makes a Wisdom save or rolls on the “Extreme Cold Mental Table”', //TODO: Add roll table to website
 	// 28
-	'Everyone makes a Frost Check or rolls on the “Extreme Cold Physical Table”',
+	'Everyone makes a Frost Check or rolls on the “Extreme Cold Physical Table”', //TODO: Add roll table to website
 	// 29
 	'You find a baby dangerous creature (Overprotective mother attacks, becomes companion?)',
 	// 30
-	'One of your beasts / party members gets sick / cant travel till recovered (medicine check to recover them?)',
+	'One of your beasts or party members gets sick / cant travel till recovered (medicine check to recover them?)',
 	// 31
 	'1d6 worth of food rations become inedible for each party member',
 	// 32
@@ -71,9 +71,9 @@ export const dangerousTravelEncounters = [
 	// 34
 	'An abandoned wagon off to the side (Supplies inside? It’s a trap?!)',
 	// 35
-	`corpses of dead animals partially burried in a strange fassion are arranged from smallest to largest (human size creature looks to be next?)`,
+	`corpses of dead animals partially burried in a strange fashion are arranged from smallest to largest (human size creature looks to be next?)`,
 	// 36
-	`Ice castle with frozen blood and frozen solid bodies (Lair of Nightmare Creature?)`,
+	`Ice castle with frozen blood and frozen solid bodies (lair of "The Spine Demon" (Abominable Yeti)?)`,
 	// 37
 	'Massive flying creature / bird / dragon flies overhead',
 	// 38
@@ -83,26 +83,26 @@ export const dangerousTravelEncounters = [
 	// 40
 	'Skelleton of a huge creature sticks out of the ground forming a large canopy (safety or a creatures lair?)',
 	// 41
-	`You find a large cavern face with dark blood across it,	inside you find a singular white scaled egg... Where is	the mother, what is this creature? (White dragon?)`,
+	`You find a large cavern face with dark blood across it,	inside you find a singular white scaled egg... Where is the mother? What is this creature? (White dragon?)`,
 ]
 
 export const safeTravelEncounters = [
 	// 1
-	'A family of 4 huddled together... frozen to death',
+	'A mile wide frozen lake with one gigantic creature frozen inside',
 	// 2
-	'A frigid battle field with bodies frozen in place',
-	// 3
-	'A 500 ft high wall that is 300 ft thick and is over 3 miles long',
-	// 4
-	'A 50ft tall statue of a dwarf holding his palm up, as if to forbid travelers from coming this way',
-	// 5
-	'A tiny island in the middle of a large frigid water lake (Whats on the island? Is the lake frozen?',
-	// 6
 	'A mile wide frozen lake with bodies frozen inside',
-	// 7
+	// 3
 	'A massive block of ice with a dangerous creature frozen	inside',
+	// 4
+	'A family of 4 huddled together... frozen to death',
+	// 5
+	'A frigid battle field with bodies frozen in place',
+	// 6
+	'A 500 ft high wall that is 300 ft thick and is over 3 miles long',
+	// 7
+	'A 50ft tall statue of a dwarf. It is holding his hand up as if to forbid travelers from coming this way',
 	// 8
-	'A mile wide frozen lake with one singular creature frozen inside',
+	'A tiny island in the middle of a large frigid water lake (Whats on the island? Is the lake frozen?',
 	// 9
 	'A massive block of ice with a gentle creature frozen inside',
 	// 10
@@ -125,13 +125,13 @@ export const safeTravelEncounters = [
 		a white haired little girl, the hunters arrive and say shes a witch
 		that needs to be dealt with`,
 	// 16
-	'A Half-Orc Druid is tending to a creature (another druid?) they have gift for players helping',
+	'A Half-Orc Druid is tending to a creature (another druid?) they have a gift for any players who help',
 	// 17
 	`Person looking for help towards where PC’s are going`,
 	// 18
-	'An injured “dangerous” animal lies in their path (shapeshifted druid?)',
+	'An injured “dangerous” creature lies in their path (shapeshifted druid?)',
 	// 19
-	'Tribal nomads that react to the party however they act to them (Fight = fight or welcoming = welcoming)',
+	'Tribal nomads that react to the party in the way that the party acts toward them (Fight = hostile or welcoming = friendly)',
 	// 20
 	'Someone appraoches who is freezing to death and needs food / warmth / shelter',
 	// 21
@@ -139,25 +139,25 @@ export const safeTravelEncounters = [
 	// 22
 	'A single old man traveling alone... hes quite strange',
 	// 23
-	'A suspicious hooded figure gathering herbs/ spell	components?',
+	'A suspicious hooded figure gathering herbs / spell components?',
 	// 24
 	'A group of fisherman struggling to catch knucklehead	trout (they are too weak)',
 	// 25
 	'Person coming from where PC’s are heading',
 	// 26
-	'A small cute creature befriends one of the PC’s and is their new familiar (what unique thing can it do?)',
+	'A small cute creature jumps into the arms of one of the PC’s (is there a unique thing can it do?)',
 	// 27
 	'A person offering help towards where the party is going',
 	// 28
-	'A traveling cartographer roams the area and will help for a price',
+	'A traveling cartographer roams the area and will help guide the party to their destination for a price',
 	// 29
-	'A cleric decked out in Cold Weather Gear offers aid to wandering adventurers (Cures stacks of exhaustion?)',
+	'A cleric decked out in Cold Weather Gear offers to aid wandering adventurers (Cures stacks of exhaustion?)',
 	// 30
-	'“Polar Bear Club” doing ice water swimming... they are crazy!',
+	'You see the “Diving Seals Club” currently swimming in nearby ice water... they are crazy!',
 	// 31
-	'A hot spring of some kind of thermal vent warming a small body of water',
+	'A hot spring or some kind of thermal vent warms a small body of water',
 	// 32
 	'An adventurer frozen in a block of ice with a magic item on them',
 	// 33
-	'Find a magical item buried in the snow/ trapped in ice',
+	'Find a magical item buried in the snow / trapped in ice',
 ]
