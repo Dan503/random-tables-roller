e7d37b0ca8ccc1da88da1e4e7ea49a94d870472f
Daniel Tonon <dan503.npm@gmail.com>
2021-06-22 22:33:46 +1000

    Removing core web vitals JS
    Not required for this little private project

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import App from './App'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
 
 render(
 	<StrictMode>
@@ -10,8 +9,3 @@ render(
 	</StrictMode>,
 	document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
