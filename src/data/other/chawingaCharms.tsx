e97f3f9f88c82d55c381243352b898812ebe650b
Daniel Tonon <dan503.npm@gmail.com>
2021-08-08 20:41:45 +1000

    Moving Charm of Animal Conjuring to the top of list

--- src/data/other/chawingaCharms.tsx
+++ src/data/other/chawingaCharms.tsx
@@ -1,173 +1,6 @@
 export const chawingaCharms = [
 	<>
 		<p>
-			<strong>Charm of Biting Cold.</strong>
-		</p>
-		<p>
-			This charm has 3 charges. As a bonus action, you can expend 1 of the
-			charm’s charges to wreathe your weapon attacks with biting cold for 1
-			minute. Until this effect ends, you deal an extra 1d6 cold damage when you
-			hit with a melee or ranged weapon attack. Once all its charges have been
-			expended, this charm vanishes from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Vitality.</strong>
-		</p>
-		<p>
-			This charm allows you to give yourself the benefit of a{' '}
-			<a href="https://www.dndbeyond.com/magic-items/potion-of-vitality">
-				Potion of Vitality
-			</a>{' '}
-			as an action. Once you do so, the charm vanishes from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Bounty.</strong>
-		</p>
-		<p>
-			This charm has 3 charges. As an action, you can expend 1 of the charm’s
-			charges to cast the{' '}
-			<a href="https://www.dndbeyond.com/spells/create-food-and-water">
-				create food and water
-			</a>{' '}
-			spell, requiring no components. Once all its charges have been expended,
-			this charm vanishes from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Cold Resistance.</strong>
-		</p>
-		<p>
-			This charm allows you to give yourself resistance to cold damage as an
-			action. This benefit lasts for 24 hours, after which the charm vanishes
-			from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Snowball Strike.</strong>
-		</p>
-		<p>
-			This charm has 5 charges. As a bonus action, you can expend 1 of the
-			charm’s charges to create a magical snowball in your hand and throw it.
-			You must have a hand free to hold the snowball, or the charge is wasted.
-			The snowball is a magic ranged weapon with which you are proficient. It
-			has a normal range of 20 feet and a long range of 60 feet, it deals 1d4
-			cold damage on a hit, and it scores a critical hit on a roll of 19 or 20.
-			If you score a critical hit with the snowball, the target is blinded until
-			the end of its next turn. The snowball vanishes immediately after you hit
-			or miss with it, or at the end of your turn if you don’t throw it. Once
-			all its charges have been expended, this charm vanishes from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of the Ice Troll.</strong>
-		</p>
-		<p>
-			This charm allows you to use your reaction when you take cold damage to
-			reduce the damage to 0. You regain a number of hit points equal to half
-			the cold damage you would have taken. Once you do so, the charm vanishes
-			from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of the Traveler’s Haven.</strong>
-		</p>
-		<p>
-			This charm has 3 charges. As an action, you can expend 1 of the charm’s
-			charges to cast the{' '}
-			<a href="https://www.dndbeyond.com/spells/leomunds-tiny-hut">
-				Leomund’s tiny hut spell
-			</a>
-			, no components required. Once all its charges have been expended, this
-			charm vanishes from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Darkvision.</strong>
-		</p>
-		<p>
-			This charm allows you to cast the{' '}
-			<a href="https://www.dndbeyond.com/spells/darkvision">darkvision</a> spell
-			as an action, no components required. Once used three times, the charm
-			goes away.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Feather Falling.</strong>
-		</p>
-		<p>
-			This charm grants you the benefits of a{' '}
-			<a href="https://www.dndbeyond.com/magic-items/ring-of-feather-falling">
-				ring of feather falling
-			</a>
-			. These benefits last for 10 days, after which the charm vanishes from
-			you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Heroism.</strong>
-		</p>
-		<p>
-			This charm allows you to give yourself the benefit of a{' '}
-			<a href="https://www.dndbeyond.com/magic-items/potion-of-heroism">
-				Potion of Heroism
-			</a>
-			as an action. Once you do so, the charm vanishes from you.
-		</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of Restoration.</strong>
-		</p>
-		<p>
-			This charm has 6 charges. You can use an action to expend some of its
-			charges to cast one of the following spells:
-		</p>
-		<ul>
-			<li>
-				<a href="https://www.dndbeyond.com/spells/greater-restoration">
-					Greater Restoration
-				</a>{' '}
-				(4 charges)
-			</li>
-			<li>
-				<a href="https://www.dndbeyond.com/spells/lesser-restoration">
-					Lesser Restoration
-				</a>{' '}
-				(2 charges)
-			</li>
-		</ul>
-		<p>Once all its charges have been expended, the charm vanishes from you.</p>
-	</>,
-	<>
-		<p>
-			<strong>Charm of the Slayer.</strong>
-		</p>
-		<p>
-			One sword in your possession becomes a{' '}
-			<a href="https://www.dndbeyond.com/magic-items/dragon-slayer">
-				Dragon Slayer
-			</a>{' '}
-			or{' '}
-			<a href="https://www.dndbeyond.com/magic-items/giant-slayer">
-				Giant Slayer
-			</a>{' '}
-			(DM’s choice) for the next 9 days. The charm then vanishes from you, and
-			the weapon returns to normal.
-		</p>
-	</>,
-	<>
-		<p>
 			<strong>Charm of Animal Conjuring.</strong>
 		</p>
 		<p>
@@ -512,6 +345,174 @@ export const chawingaCharms = [
 			</table>
 		</div>
 	</>,
+	<>
+		<p>
+			<strong>Charm of Biting Cold.</strong>
+		</p>
+		<p>
+			This charm has 3 charges. As a bonus action, you can expend 1 of the
+			charm’s charges to wreathe your weapon attacks with biting cold for 1
+			minute. Until this effect ends, you deal an extra 1d6 cold damage when you
+			hit with a melee or ranged weapon attack. Once all its charges have been
+			expended, this charm vanishes from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Vitality.</strong>
+		</p>
+		<p>
+			This charm allows you to give yourself the benefit of a{' '}
+			<a href="https://www.dndbeyond.com/magic-items/potion-of-vitality">
+				Potion of Vitality
+			</a>{' '}
+			as an action. Once you do so, the charm vanishes from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Bounty.</strong>
+		</p>
+		<p>
+			This charm has 3 charges. As an action, you can expend 1 of the charm’s
+			charges to cast the{' '}
+			<a href="https://www.dndbeyond.com/spells/create-food-and-water">
+				create food and water
+			</a>{' '}
+			spell, requiring no components. Once all its charges have been expended,
+			this charm vanishes from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Cold Resistance.</strong>
+		</p>
+		<p>
+			This charm allows you to give yourself resistance to cold damage as an
+			action. This benefit lasts for 24 hours, after which the charm vanishes
+			from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Snowball Strike.</strong>
+		</p>
+		<p>
+			This charm has 5 charges. As a bonus action, you can expend 1 of the
+			charm’s charges to create a magical snowball in your hand and throw it.
+			You must have a hand free to hold the snowball, or the charge is wasted.
+			The snowball is a magic ranged weapon with which you are proficient. It
+			has a normal range of 20 feet and a long range of 60 feet, it deals 1d4
+			cold damage on a hit, and it scores a critical hit on a roll of 19 or 20.
+			If you score a critical hit with the snowball, the target is blinded until
+			the end of its next turn. The snowball vanishes immediately after you hit
+			or miss with it, or at the end of your turn if you don’t throw it. Once
+			all its charges have been expended, this charm vanishes from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of the Ice Troll.</strong>
+		</p>
+		<p>
+			This charm allows you to use your reaction when you take cold damage to
+			reduce the damage to 0. You regain a number of hit points equal to half
+			the cold damage you would have taken. Once you do so, the charm vanishes
+			from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of the Traveler’s Haven.</strong>
+		</p>
+		<p>
+			This charm has 3 charges. As an action, you can expend 1 of the charm’s
+			charges to cast the{' '}
+			<a href="https://www.dndbeyond.com/spells/leomunds-tiny-hut">
+				Leomund’s tiny hut spell
+			</a>
+			, no components required. Once all its charges have been expended, this
+			charm vanishes from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Darkvision.</strong>
+		</p>
+		<p>
+			This charm allows you to cast the{' '}
+			<a href="https://www.dndbeyond.com/spells/darkvision">darkvision</a> spell
+			as an action, no components required. Once used three times, the charm
+			goes away.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Feather Falling.</strong>
+		</p>
+		<p>
+			This charm grants you the benefits of a{' '}
+			<a href="https://www.dndbeyond.com/magic-items/ring-of-feather-falling">
+				ring of feather falling
+			</a>
+			. These benefits last for 10 days, after which the charm vanishes from
+			you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Heroism.</strong>
+		</p>
+		<p>
+			This charm allows you to give yourself the benefit of a{' '}
+			<a href="https://www.dndbeyond.com/magic-items/potion-of-heroism">
+				Potion of Heroism
+			</a>
+			as an action. Once you do so, the charm vanishes from you.
+		</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of Restoration.</strong>
+		</p>
+		<p>
+			This charm has 6 charges. You can use an action to expend some of its
+			charges to cast one of the following spells:
+		</p>
+		<ul>
+			<li>
+				<a href="https://www.dndbeyond.com/spells/greater-restoration">
+					Greater Restoration
+				</a>{' '}
+				(4 charges)
+			</li>
+			<li>
+				<a href="https://www.dndbeyond.com/spells/lesser-restoration">
+					Lesser Restoration
+				</a>{' '}
+				(2 charges)
+			</li>
+		</ul>
+		<p>Once all its charges have been expended, the charm vanishes from you.</p>
+	</>,
+	<>
+		<p>
+			<strong>Charm of the Slayer.</strong>
+		</p>
+		<p>
+			One sword in your possession becomes a{' '}
+			<a href="https://www.dndbeyond.com/magic-items/dragon-slayer">
+				Dragon Slayer
+			</a>{' '}
+			or{' '}
+			<a href="https://www.dndbeyond.com/magic-items/giant-slayer">
+				Giant Slayer
+			</a>{' '}
+			(DM’s choice) for the next 9 days. The charm then vanishes from you, and
+			the weapon returns to normal.
+		</p>
+	</>,
+
 	<>
 		<p>
 			<strong>Charm of the Snow Walker.</strong>
